0a93684825600a17af627c2790f7ca96a0867f92
brant <403216075@qq.com>
2021-02-20 14:50:07 +0800

    [feat]两个包同时修改

--- packages/hong-lerna-core/index.js
+++ packages/hong-lerna-core/index.js
@@ -1,5 +1,8 @@
 const API = "https://yande.re/post/popular_recent.json";
 
+const test = "nihao!!!!";
+
 module.exports = {
   API,
+  test,
 };
--- packages/hong-lerna-popular/index.js
+++ packages/hong-lerna-popular/index.js
@@ -1,9 +1,11 @@
-const { API } = require("hong-lerna-core");
+const { API, test } = require("hong-lerna-core");
 const axios = require("axios");
 
 const getPopularImg = () => axios.get(API);
 
 module.exports = getPopularImg;
+
+console.log(test);
 
 // 测试代码，发布时删除
 getPopularImg().then((res) => console.log(res.data.length));
